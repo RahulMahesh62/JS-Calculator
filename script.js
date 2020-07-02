211b5088372d46bfa180468d645f54c25709eff8
Rahul Mahesh <rahulmahesh62@gmail.com>
2020-07-02 23:41:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,13 @@ function getOutput() {
     return document.getElementById("output-value").innerText;
 }
 
+
+function getFormattedNumber(num) {
+    var n = Number(num);
+    var value = n.toLocaleString("en");
+    return value;
+}
+
 function printOutput(num) {
     if (num == "") {
         document.getElementById("output-value").innerText = num;
@@ -18,12 +25,34 @@ function printOutput(num) {
     }
 }
 
-function getFormattedNumber(num) {
-    var n = Number(num);
-    var value = n.toLocaleString("en");
-    return value;
-}
-
 function reverseNumberFormat(num) {
     return Number(num.replace(/,/g, ''));
+}
+
+var operator = document.getElementsByClassName("operator");
+for (var i = 0; i < operator.length; i++) {
+    operator[i].addEventListener('click', function() {
+        if (this.id == "clear") {
+            printOutput("");
+            printHistory("");
+        }
+        if (this.id == "backspace") {
+            var output = reverseNumberFormat(getOutput()).toString();
+            if (output) {
+                output = output.substr(0, output.length - 1);
+                printOutput(output);
+            }
+        }
+    })
+}
+
+var number = document.getElementsByClassName("number");
+for (var i = 0; i < number.length; i++) {
+    number[i].addEventListener('click', function() {
+        var output = reverseNumberFormat(getOutput());
+        if (output != NaN) {
+            output = output + this.id;
+            printOutput(output);
+        }
+    })
 }
