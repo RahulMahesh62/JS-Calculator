affc067040704dc9be45efa5c76fcdac0bbc093e
Rahul Mahesh <rahulmahesh62@gmail.com>
2020-07-02 23:25:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function getHistory() {
+    return document.getElementById("history-value").innerText;
+}
+
+function printHistory(num) {
+    document.getElementById("history-value").innerText = num;
+}
+
+function getOutput() {
+    return document.getElementById("output-value").innerText;
+}
+
+function printOutput(num) {
+    if (num == "") {
+        document.getElementById("output-value").innerText = num;
+    } else {
+        document.getElementById("output-value").innerText = getFormattedNumber(num);
+    }
+}
+
+function getFormattedNumber(num) {
+    var n = Number(num);
+    var value = n.toLocaleString("en");
+    return value;
+}
+
+function reverseNumberFormat(num) {
+    return Number(num.replace(/,/g, ''));
+}
